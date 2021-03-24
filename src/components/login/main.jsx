92c7b7d2e7e33727f2c674c42db293d5282dac76
roseburrow <thomas.brewer@protonmail.com>
2021-03-24 13:38:44 +0000

    Langauge Callbacks Reconfigured as Promises

--- src/components/login/main.jsx
+++ src/components/login/main.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react';
 import LoginForm from '../login-form/main';
 import LoginHeader from '../login-header/main';
 
-import { FALLBACK_LANG, getLanguageJSON } from '../../functions/language';
+import { loadLanguageConfig } from '../../functions/language';
 
 import './login.css';
 
@@ -13,21 +13,8 @@ function Login(props) {
 	const [languageConfig, setLanguageConfig] = useState({});
 
 	useEffect(() => {
-		getLanguageConfig();
+		loadLanguageConfig(language, (langConfig) => setLanguageConfig(langConfig));
 	}, [ language ]);
-
-	function getLanguageConfig() {
-		//Load selected lang json object.
-		getLanguageJSON('login', language, (langObj) => {
-
-			//Load the fallback lang json object incase any strings are missing from the main lang object.
-			getLanguageJSON('login', FALLBACK_LANG, (fallbackLangObj) => {
-				
-				//Merging them this way means any keys that don't exist in one but do in the other get resolved nicely.
-				setLanguageConfig({ ...fallbackLangObj, ...langObj });
-			});
-		});
-	}
 
 	return (
 		<div>
--- src/functions/language.js
+++ src/functions/language.js
@@ -1,22 +1,38 @@
 export const FALLBACK_LANG = 'english';
 
-export function getLanguageJSON(page, language, callback) {
-	let xhttp = new XMLHttpRequest();
-	let file = `lang/${ page }/${language}.json`;
+export function getLanguageJSON(page, language) {
+	return new Promise((resolve, reject) => {
+		let xhttp = new XMLHttpRequest();
+		let file = `lang/${ page }/${language}.json`;
 
-	xhttp.onreadystatechange = function () {
-		if (this.readyState == 4 && this.status == 200) {
-			console.log(this.responseText)
-			try {
-				const LANGUAGE_JSON = JSON.parse(this.responseText);
-				callback(LANGUAGE_JSON);
+		xhttp.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				try {
+					const LANGUAGE_JSON = JSON.parse(this.responseText);
+					resolve(LANGUAGE_JSON);
+				}
+				catch (e) {
+					reject(e);
+				}
 			}
-			catch (e) {
-				console.log("Error Parsing Language JSON: " + e);
-			}
-		}
-	};
+		};
 
-	xhttp.open("GET", file, false);
-	xhttp.send();
+		xhttp.open("GET", file, true);
+		xhttp.send();
+	});
+}
+
+export function loadLanguageConfig(language, callback) {
+	Promise.all([
+		getLanguageJSON('login', language),
+		getLanguageJSON('login', FALLBACK_LANG)
+	]).then((configs) => {
+		let finalConfig = {};
+
+		configs.forEach((config) => {
+			finalConfig = { ...config, ...finalConfig }
+		});
+
+		callback(finalConfig);
+	}).catch((error) => console.log("Error Loading Language:" + error));
 }
